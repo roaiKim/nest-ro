48caa6606adaacd05718dd74de951d4d4b08f723
roaiKim <shcroai@gmail.com>
2020-07-27 00:53:51 +0800

    update

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,11 @@ import {join} from 'path';
 
 export default {
     type: 'mysql',
+    host: 'rm-wz9bnr1qt0xvgyd5b9o.mysql.rds.aliyuncs.com',
+    port: 3306,
+    username: 'roaikim',
+    password: '953507353aliy',
+    database: 'roaikim',
     entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')],
     synchronize: true
 }
