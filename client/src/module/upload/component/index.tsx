4c07cc4c06f64f15953e566702be113b3a75dc0b
roaiKim <shcroai@gmail.com>
2021-04-06 22:39:07 +0800

    update

--- client/src/module/upload/component/index.tsx
+++ client/src/module/upload/component/index.tsx
@@ -3,6 +3,7 @@ import { Upload, message, Button } from 'antd';
 import { Store } from 'antd/lib/form/interface';
 import { UploadChangeParam } from 'antd/lib/upload';
 import { UploadOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 class Main extends React.PureComponent {
   submit = (value: Store) => {
@@ -13,12 +14,41 @@ class Main extends React.PureComponent {
     console.log(info);
   };
 
+  down = () => {
+    console.log("opone")
+    axios({    
+      method: 'get',    
+      url: "http://localhost:3000/api/stream/download",    
+      data: {      
+        test: "test data"    
+      },
+      responseType: "arraybuffer"
+    }).then(response => {      
+        console.log(response.data)
+        const blob = new Blob([response.data], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          // type: "application/x-msdownload"
+        })
+
+        const url = URL.createObjectURL(blob);
+        const ele = document.createElement("a");
+        ele.style.display = "none";
+        ele.href = url;
+        ele.download = "xlsx"
+        ele.click()
+        ele.remove()
+    })
+  }
+
   render() {
     return (
       <div className="">
         <Upload name="file" action="/api/file/uploadoss" onChange={this.onChange}>
           <Button icon={<UploadOutlined />}>单文件上传图片</Button>
         </Upload>
+        <div>
+          <button onClick={this.down}>文件stream下载</button>
+        </div>
       </div>
     );
   }
--- service/src/main.ts
+++ service/src/main.ts
@@ -18,13 +18,15 @@ async function bootstrap() {
 
   // app.use(history()); // history-api-fallback
 
+  app.enableCors();
+  
   await app.listen(3000);
-
-  if (process.env.NODE_ENV === "prod") {
-    console.log("rios")
-  }
 }
 
 bootstrap();
+
+if (process.env.NODE_ENV === "dev") {
+    console.log("dev 环境")
+}
 
 console.info("\n程序已启动", "端口为3000\n")
